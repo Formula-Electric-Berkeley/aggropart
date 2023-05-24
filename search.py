da66071aa426dfb865c828d20992c0631d658ee9
Boomaa23 <cmo93003@yahoo.com>
2023-05-23 22:41:46 -0700

    Finish Altium BOM parsing

--- search.py
+++ search.py
@@ -3,35 +3,147 @@
 TODO add description
 """
 
+# input Altium BOM
+# validates all rows have Mouser, Digikey, JLC part numbers (or warn to ignore)
+# Do inventory search (Notion) to see which parts are already available (& remove)
+#  if None or multiple, ask user which is correct
+#  if no box, warn & provide link for user - option for manual override
+#  Subtract quantities from inventory if needed (& arg is passed)
+# If assembly flag passed, (and API key provided)
+#  search JLC and subtract all that are present
+#  if a part is OOS, ask for alt part num or to buy externally
+#  warn for extended parts (?)
+#  tally part cost, parts to order from JLC
+# For all remaining parts,
+#  provide digikey and mouser part costs (for diff groupings)
+#  user selects each src & qty (unless part only exists on one)
+#  create importable BOM for Mouser and Digikey separately at end
+
 import argparse
 import csv
+import dotenv
+import os
 import sys
+
+import notion_client
+
+
+optional_bom_fields = {
+    'Description': {},      # For manual review only
+    'Designator': {},       # For manual review only
+    # 'Revision Status': {}, #TODO doesn't work?
+    'JLCPCB Part Type': {}  # Only matters for JLC price esimation
+}
+
+required_bom_fields = {
+    'Comment': {
+        'notion': 'Part Number/title/plain_text'
+    },
+    'Quantity': {
+        'notion': 'Current Quantity/number'
+    },
+    'Digi-Key Part Number': {},
+    'Mouser Part Number': {},
+    'JLCPCB Part Number': {}
+}
 
 
 def main(args):
+    if not os.path.exists(args.bom):
+        raise FileNotFoundError(args.bom)
+    
+    if not args.bom.lower().endswith('.csv'):
+        raise ValueError(f'Filepath "{args.bom}" does not end in .csv')
+    
+    with open(args.bom, 'r') as bom_fh:
+        bom = parse_altium_bom(bom_fh)
+        check_inventory(bom, args.search_inventory, args.decrement_inventory)
+        check_jlc(bom, args.assembly)
+        check_digikey_mouser(bom)
+
+
+def parse_altium_bom(fh):
+    """Read and parse a BOM exported from Altium given a file handle."""
+    bom_csv = csv.reader(fh)
+    try:
+        headers = next(bom_csv)
+    except StopIteration:
+        # Check that the header was read - i.e. the file is not already empty
+        raise ValueError('BOM did not contain enough data / the data was invalid')
+    
+    header_col_map = []
+    for header in required_bom_fields.keys():
+        # Validate that all headers are in the provided BOM
+        if header not in headers:
+            raise ValueError(f'"{header}" was not found as a header in the BOM')
+        # Create a mapping of header name to column index
+        header_col_map.append(headers.index(header))
+    
+    for header in optional_bom_fields.keys():
+        if header not in headers:
+            # Fields are not required, so ignore them if unavailable
+            continue
+        header_col_map.append(headers.index(header))
+        
+    bom = []
+    for row in bom_csv:
+        # Skip rows that are blank/empty
+        if len(row) == 0:
+            continue
+        bom_item = []
+        bom.append(bom_item)
+        # Reorder CSV data into template format
+        for col in header_col_map:
+            bom_item.append(row[col])
+    return bom
+
+
+def check_inventory(bom, search, decrement):
+    if not search:
+        return
+    
+    client = notion_client.Client(auth=os.environ['NOTION_TOKEN'])
+    inventory_raw = client.databases.query(os.environ['NOTION_DB_ID'])
+    #TODO inventory caching
+    inventory = []
+    for item in inventory_raw:
+        properties = item['properties']
+        # Equivalent to a blank Project property field
+        if properties['📽️ Projects']['id'] == 'fdLi':
+            pass
+        #TODO finish this
+
+def check_jlc(bom, assembly):
+    if not assembly:
+        return
+    
+def check_digikey_mouser(bom):
     pass
 
 
 def _parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    # input Altium BOM
-    #validates all rows have Mouser, Digikey, JLC part numbers (or warn to ignore)
-    # Do inventory search (Notion) to see which parts are already available (& remove)
-    #  if None or multiple, ask user which is correct
-    #  if no box, warn & provide link for user - option for manual override
-    #  Subtract quantities from inventory if needed (& arg is passed)
-    # If assembly flag passed, (and API key provided)
-    #  search JLC and subtract all that are present
-    #  if a part is OOS, ask for alt part num or to buy externally
-    #  warn for extended parts (?)
-    #  tally part cost, parts to order from JLC
-    # For all remaining parts,
-    #  provide digikey and mouser part costs (for diff groupings)
-    #  user selects each src & qty (unless part only exists on one)
-    #  create importable BOM for Mouser and Digikey separately at end
-
-
+    parser.add_argument('bom', help='filepath of Bill of Materials exported from Altium')
+    parser.add_argument('--search-inventory', '-s',
+                        action='store_true', 
+                        default=True,
+                        help='search the inventory for existing parts')
+    parser.add_argument('--decrement-inventory', '-d',
+                        action='store_true',
+                        default=False,
+                        help='decrement quantity of inventory items used in BOM')
+    parser.add_argument('--assembly', '-a', 
+                        action='store_true',
+                        default=False,
+                        help='use JLCPCB assembly service and parts')
+    #TODO validate
+    parser.add_argument('--validate-only', '-v',
+                        action='store_true',
+                        default=False,
+                        help='validate that the BOM contains the right data ONLY')
+    return parser.parse_args()
 
 if __name__ == "__main__":
+    dotenv.load_dotenv()
     args = _parse_args()
     sys.exit(main(args))
